fa1b659e900f45ac549914672becf9ad57ac4ffa
AdamClifford94 <adam.clifford@hotmail.co.uk>
2020-02-25 17:01:54 +0000

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String p = "test test test";
+        System.out.println(p);
+        System.out.println( "Hello World!" );
+    }
+}
